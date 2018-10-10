d9251a2d2a21c3706b6b7da34a9cfa40607ac51d
ll9 <llwierer@web.de>
2018-10-10 21:29:02 +0200

    Show Progress in textbox

--- ProgressExample/Form1.Designer.cs
+++ ProgressExample/Form1.Designer.cs
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ProcessButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ProcessButton
+            // 
+            this.ProcessButton.Location = new System.Drawing.Point(213, 48);
+            this.ProcessButton.Name = "ProcessButton";
+            this.ProcessButton.Size = new System.Drawing.Size(106, 23);
+            this.ProcessButton.TabIndex = 0;
+            this.ProcessButton.Text = "Start Processing";
+            this.ProcessButton.UseVisualStyleBackColor = true;
+            this.ProcessButton.Click += new System.EventHandler(this.ProcessButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(213, 123);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(355, 403);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ProcessButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ProcessButton;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- ProgressExample/Form1.cs
+++ ProgressExample/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,30 @@ namespace ProgressExample
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void DoProcessing(IProgress<int> progress)
+        {
+            for (int i = 0; i != 100; ++i)
+            {
+                Thread.Sleep(100); // CPU-bound work
+                if (progress != null)
+                    progress.Report(i);
+            }
+        }
+
+        private async void ProcessButton_Click(object sender, EventArgs e)
+        {
+            // The Progress<T> constructor captures our UI context,
+            //  so the lambda will be run on the UI thread.
+            var progress = new Progress<int>(percent =>
+            {
+                textBox1.Text = percent + "%";
+            });
+
+            // DoProcessing is run on the thread pool.
+            await Task.Run(() => DoProcessing(progress));
+            textBox1.Text = "Done!";
         }
     }
 }
